257271de21efc925f624f821bf33ff5ab4e1692a
Enrique Palacios Baron <71736745+enriquepalaciosb@users.noreply.github.com>
2021-11-17 13:00:12 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+//jshint esversion:6
+
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+var myName = superheroes.random();
+var myVillainName = supervillains.random();
+console.log(myName);
+console.log(myVillainName);
